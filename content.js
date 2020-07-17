8765827edf766e81eb9ef513591f42c72bab44fc
Mike Mazzola <mazzola@spotify.com>
2020-07-17 10:33:19 -0500

    Send dice rolls to Discord
    Send the dice roll from the character sheet to the Discord channel using the Webhook API. Using Webhooks is easier than using the full-fledged Bot and Gateway API for Discord.
    
    Let the user specify which webhook URL to use when sending messages. Instead of hardcoding the URL in the codebase, which is both unsecure and limiting, the user is able to go into the extension's Options page and specify a URL themselves.

--- content.js
+++ content.js
@@ -1,22 +1,35 @@
+var webhookUrl
+chrome.storage.sync.get('webhookUrl', data => webhookUrl = data.webhookUrl)
+
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
-var observer = new MutationObserver(logDiceResult)
+var observer = new MutationObserver(sendDiceResult)
 observer.observe(document.body, {
 	childList: true,
 	subtree: true
 })
 
-function logDiceResult(mutations, observer) {
+function sendDiceResult(mutations, observer) {
 	for(const mutation of mutations) {
 		let diceRollMutation = mutation.target.id == "noty_layout__bottomRight"
-		let isNewDiceRoll = mutation.addedNodes.length > 0 && mutation.addedNodes.item(0).id.startsWith("noty_bar")
+		let isNewDiceRoll = mutation.addedNodes.item(0)?.id?.startsWith("noty_bar")
 		if (diceRollMutation && isNewDiceRoll) {
 			let diceResults = document.body.querySelectorAll(".dice_result__total-result")
 			let latestDiceResult = diceResults.item(diceResults.length - 1)
 			if (latestDiceResult !== null) {
-				console.log(latestDiceResult.innerText)
+				sendMessageToDiscordChannel(latestDiceResult.innerText)
 				break;
 			}
 		}
 	}
+}
+
+function sendMessageToDiscordChannel(diceResult) {
+	let message = {"content": `${characterName()} rolled a ${diceResult}`}
+	let headers = {"content-type": "application/json"}
+	fetch(webhookUrl, {"method": "POST", "headers": headers, "body": JSON.stringify(message)})
+}
+
+function characterName() {
+	return document.body.querySelector(".ddbc-character-name ").innerText
 }
---
+++ options.js
@@ -0,0 +1,32 @@
+const webhookUrlField = document.getElementById("webhookUrlField")
+chrome.storage.sync.get('webhookUrl', data => setWebhookInputField(data))
+document.getElementById("saveButton").addEventListener("click", storeWebhookUrl);
+
+function setWebhookInputField(data) {
+	console.log(`Setting: ${data.webhookUrl}`)
+	webhookUrlField.value = data.webhookUrl
+}
+
+function storeWebhookUrl() {
+	let value = htmlEncode(webhookUrlField.value)
+	if (isValidUrl(value)) {
+		console.log(`Storing: ${webhookUrlField.value}`)
+		chrome.storage.sync.set({webhookUrl: webhookUrlField.value})	
+	}
+}
+
+function isValidUrl(string) {
+  try {
+    new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return true;
+}
+
+function htmlEncode(string) {
+  var element = document.createElement('element');
+  element.innerText = string;
+  return element.innerHTML;
+}
