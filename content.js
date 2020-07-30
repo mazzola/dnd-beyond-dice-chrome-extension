332f185407b8798310af49c7294f7ea1973a102c
Mike Mazzola <mazzola@spotify.com>
2020-07-30 13:49:17 -0500

    Add character image and roll info to message
    Creates a Discord message with an embed object which will display the character's profile image, the dice roll description, breakdown, result, and notation.
    
    Providing a profile image and roll description allows the results to be more readable when multiple players are rolling at once. Displaying the dice roll breakdown also provides more flexibility to the players and DM because it allows them to apply house rules to things like natural 20s and 1s.

--- content.js
+++ content.js
@@ -25,11 +25,50 @@ function sendDiceResult(mutations, observer) {
 }
 
 function sendMessageToDiscordChannel(diceResult) {
-	let message = {"content": `${characterName()} rolled a ${diceResult}`}
+	let message = {
+		"embeds": [{
+			"title": createMessageTitle(),
+			"description": createMessageDescription(diceResult),
+			"color": 12923185,
+			"thumbnail": createMessageThumbnail(),
+			"footer": createMessageFooter()
+		}]
+	}
 	let headers = {"content-type": "application/json"}
 	fetch(webhookUrl, {"method": "POST", "headers": headers, "body": JSON.stringify(message)})
 }
 
+function createMessageTitle() {
+	let rollDescriptions = document.body.querySelectorAll(".dice_result__info__title")
+	let latestRollDescription = last(rollDescriptions).innerText
+	return `${characterName()} rolled ${capitalize(latestRollDescription)}`
+}
+
+function createMessageDescription(diceResult) {
+	let diceBreakdowns = document.body.querySelectorAll(".dice_result__info__breakdown")
+	let latestDiceBreakdown = last(diceBreakdowns).innerText
+	return `${latestDiceBreakdown} = \`${diceResult}\``
+}
+
+function createMessageThumbnail() {
+	let portrait = document.body.querySelector(".ddbc-character-avatar__portrait")
+	let portraitUrl = portrait.style.backgroundImage.slice(5, -2)
+	return {"url": portraitUrl}
+}
+
+function createMessageFooter() {
+	let diceNotations = document.body.querySelectorAll(".dice_result__info__dicenotation")
+	return {"text": last(diceNotations).innerText}
+}
+
 function characterName() {
 	return document.body.querySelector(".ddbc-character-name ").innerText
+}
+
+function last(nodeList) {
+	return nodeList.item(nodeList.length - 1)
+}
+
+function capitalize(words) {
+	return words.replace(/\w\S*/g, word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
 }
