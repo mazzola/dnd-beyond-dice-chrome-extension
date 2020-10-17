85acf560fe1184dc86d95d319960f59214ca4173
miek <mike.fleming.dev@gmail.com>
2020-10-17 00:08:28 -0500

    feat(parity): react app reached feature parity

--- options/index.js
+++ options/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 
 import '../common/hot-reload'
 
-const Form = () => {
+const Options = () => {
     const [webhookUrl, setWebhookUrl] = useState('')
+    const handleChange = (e) => setWebhookUrl(e.target.value) // TODO: handle url validation
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log(`Storing: ${webhookUrl}`)
+        chrome.storage.sync.set({ webhookUrl })
+    }
 
     useEffect(() => {
         chrome.storage.sync.get('webhookUrl', ({ webhookUrl }) =>
-            setWebhookUrl('webhookUrl')
+            setWebhookUrl(webhookUrl)
         )
     }, [])
 
@@ -16,45 +22,11 @@ const Form = () => {
         <form>
             <label>Discord Webhook URL:</label>
             <br />
-            <input type="url" id="webhookUrlField" />
+            <input type="url" onChange={handleChange} value={webhookUrl} />
             <br />
-            <input type="submit" id="saveButton" value="Save" />
+            <input type="submit" onClick={handleClick} value="Save" />
         </form>
     )
 }
 
-ReactDOM.render(<Form />, document.getElementById('root'))
-
-// const webhookUrlField = document.getElementById('webhookUrlField')
-
-// chrome.storage.sync.get('webhookUrl', (data) => setWebhookInputField(data))
-// document.getElementById('saveButton').addEventListener('click', storeWebhookUrl)
-
-// function setWebhookInputField(data) {
-//     console.log(`Setting: ${data.webhookUrl}`)
-//     webhookUrlField.value = data.webhookUrl
-// }
-
-// function storeWebhookUrl() {
-//     let value = htmlEncode(webhookUrlField.value)
-//     if (isValidUrl(value)) {
-//         console.log(`Storing: ${webhookUrlField.value}`)
-//         chrome.storage.sync.set({ webhookUrl: webhookUrlField.value })
-//     }
-// }
-
-// function isValidUrl(string) {
-//     try {
-//         new URL(string)
-//     } catch (_) {
-//         return false
-//     }
-
-//     return true
-// }
-
-// function htmlEncode(string) {
-//     var element = document.createElement('element')
-//     element.innerText = string
-//     return element.innerHTML
-// }
+ReactDOM.render(<Options />, document.getElementById('root'))
