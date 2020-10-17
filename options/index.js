fe88f6363c41c1ccd3c6c7dd77d1b7334dfcba08
miek <mike.fleming.dev@gmail.com>
2020-10-16 23:29:28 -0500

    feat(react): react hello world

--- options/index.js
+++ options/index.js
@@ -1,46 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello!</h1>, document.getElementById('root'))
+import '../common/hot-reload'
 
-chrome.management.getSelf((self) => {
-    if (self.installType === 'development') {
-        const hmrConnection = new WebSocket('ws://localhost:8080')
+const Form = () => {
+    const [webhookUrl, setWebhookUrl] = useState('')
 
-        hmrConnection.onmessage = () => window.location.reload()
-    }
-})
+    useEffect(() => {
+        chrome.storage.sync.get('webhookUrl', ({ webhookUrl }) =>
+            setWebhookUrl('webhookUrl')
+        )
+    }, [])
 
-const webhookUrlField = document.getElementById('webhookUrlField')
-
-chrome.storage.sync.get('webhookUrl', (data) => setWebhookInputField(data))
-document.getElementById('saveButton').addEventListener('click', storeWebhookUrl)
-
-function setWebhookInputField(data) {
-    console.log(`Setting: ${data.webhookUrl}`)
-    webhookUrlField.value = data.webhookUrl
+    return (
+        <form>
+            <label>Discord Webhook URL:</label>
+            <br />
+            <input type="url" id="webhookUrlField" />
+            <br />
+            <input type="submit" id="saveButton" value="Save" />
+        </form>
+    )
 }
 
-function storeWebhookUrl() {
-    let value = htmlEncode(webhookUrlField.value)
-    if (isValidUrl(value)) {
-        console.log(`Storing: ${webhookUrlField.value}`)
-        chrome.storage.sync.set({ webhookUrl: webhookUrlField.value })
-    }
-}
+ReactDOM.render(<Form />, document.getElementById('root'))
 
-function isValidUrl(string) {
-    try {
-        new URL(string)
-    } catch (_) {
-        return false
-    }
+// const webhookUrlField = document.getElementById('webhookUrlField')
 
-    return true
-}
+// chrome.storage.sync.get('webhookUrl', (data) => setWebhookInputField(data))
+// document.getElementById('saveButton').addEventListener('click', storeWebhookUrl)
 
-function htmlEncode(string) {
-    var element = document.createElement('element')
-    element.innerText = string
-    return element.innerHTML
-}
+// function setWebhookInputField(data) {
+//     console.log(`Setting: ${data.webhookUrl}`)
+//     webhookUrlField.value = data.webhookUrl
+// }
+
+// function storeWebhookUrl() {
+//     let value = htmlEncode(webhookUrlField.value)
+//     if (isValidUrl(value)) {
+//         console.log(`Storing: ${webhookUrlField.value}`)
+//         chrome.storage.sync.set({ webhookUrl: webhookUrlField.value })
+//     }
+// }
+
+// function isValidUrl(string) {
+//     try {
+//         new URL(string)
+//     } catch (_) {
+//         return false
+//     }
+
+//     return true
+// }
+
+// function htmlEncode(string) {
+//     var element = document.createElement('element')
+//     element.innerText = string
+//     return element.innerHTML
+// }
