60010135a7aed3e43c954da9303aa7fb1eae8691
miek <mike.fleming.dev@gmail.com>
2020-10-19 08:45:01 -0500

    feat(sheet-mgmt): manages multiple characters

--- src/index.js
+++ src/index.js
@@ -1,30 +1,116 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import ReactDOM from 'react-dom'
+import { useForm, useField } from 'react-form'
+
+const initialState = {
+    characters: [],
+}
+
+const ADD_CHARACTER = 'add_character'
+const LOAD_CHARACTERS = 'load_characters'
+
+const reducer = (state, action) => {
+    console.log('in reducer....', action)
+    switch (action.type) {
+        case ADD_CHARACTER:
+            return {
+                ...state,
+                characters: [
+                    ...state.characters,
+                    { ddbUrl: '', discordUrl: '' },
+                ],
+            }
+        case 'remove_character':
+            return state
+        case 'update_character':
+            return state
+        case LOAD_CHARACTERS:
+            return {
+                ...state,
+                characters: action.payload,
+            }
+        case 'save_characters':
+            return state
+        default:
+            throw new Error(`Action type ${action.type} not supported`)
+    }
+}
 
 const Options = () => {
-    const [webhookUrl, setWebhookUrl] = useState('')
-    const handleChange = (e) => setWebhookUrl(e.target.value) // TODO: handle url validation
-    const handleClick = (e) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const handleAddCharacter = (e) => {
         e.preventDefault()
-        console.log(`Storing: ${webhookUrl}`)
-        chrome.storage.sync.set({ webhookUrl })
+        dispatch({ type: ADD_CHARACTER })
     }
+    const { Form } = useForm({
+        onSubmit: (values, instance) => {
+            const { characters } = values
+
+            console.log('submitting...', values)
+
+            chrome.storage.sync.set({
+                diceBot: { characters },
+            })
+        },
+    })
 
     useEffect(() => {
-        chrome.storage.sync.get('webhookUrl', ({ webhookUrl }) =>
-            setWebhookUrl(webhookUrl)
+        chrome.storage.sync.get('diceBot', ({ diceBot }) =>
+            dispatch({
+                type: LOAD_CHARACTERS,
+                payload: diceBot.characters,
+            })
         )
     }, [])
 
     return (
-        <form>
-            <label>Discord Webhook URL:</label>
-            <br />
-            <input type="url" onChange={handleChange} value={webhookUrl} />
-            <br />
-            <input type="submit" onClick={handleClick} value="Save" />
-        </form>
+        <>
+            <pre>{JSON.stringify(state, null, 4)}</pre>
+            <Form>
+                {state.characters.map(({ ddbUrl, discordUrl }, i) => {
+                    return (
+                        <>
+                            <Input
+                                scope="characters"
+                                fieldName="ddbUrl"
+                                index={i}
+                                defaultValue={ddbUrl}
+                            />
+                            <Input
+                                scope="characters"
+                                fieldName="discordUrl"
+                                index={i}
+                                defaultValue={discordUrl}
+                            />
+                        </>
+                    )
+                })}
+                <br />
+                <button onClick={handleAddCharacter}>Add a Character</button>
+                <br />
+                <button type="submit">Save</button>
+            </Form>
+        </>
     )
 }
 
 ReactDOM.render(<Options />, document.getElementById('root'))
+
+function Input({
+    scope,
+    fieldName,
+    defaultValue = '',
+    index,
+    inputType = 'url',
+}) {
+    const { getInputProps } = useField(`${scope}[${index}].${fieldName}`, {
+        defaultValue,
+    })
+
+    return (
+        <>
+            <label>{fieldName}</label>
+            <input type={inputType} {...getInputProps()} />
+        </>
+    )
+}
